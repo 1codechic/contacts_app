5ee73dde92eaac7e2cb474cc82a944d631c7578c
1codechic <1codechic@devchics.com>
2019-03-05 21:00:18 -0500

    created methods in the model for full_name and friendly_updated_at to use in index and show views

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,10 @@
 class Contact < ApplicationRecord
+  def friendly_updated_at
+    updated_at.strftime("%b %e, %l:%M %p")
+  end
+
+  def full_name
+    name = first_name + ' '
+    name += last_name
+  end
 end
